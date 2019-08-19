63d1088fb3f88733a92b01153195f933387307e1
ioana.cora <ioana.vladau@casomes.ro>
2019-08-19 12:30:54 +0300

    methods

---
+++ src/main/java/org/fasttrackit/Multiplication.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+
+public class Multiplication {
+
+    public int multiplication (int a, int b) {
+        System.out.println( "Multiplication for: " + a + " "+ b );
+        int multiplication = a*b;
+
+        System.out.println("Display result  " + multiplication);
+        return multiplication;
+    }
+}
---
+++ src/main/java/org/fasttrackit/SumCalculation.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+
+public class SumCalculation
+{
+    public double suma(double a, double b) {
+        System.out.println("Calculate sum for: " + a + " " + b );
+        double suma= a+b;
+        System.out.println("Display sum =" + suma);
+        return suma;
+    }
+
+}
